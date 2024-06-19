d25bc7687a0660a408e12b1f0a879dda60e29773
Geoffroy Maurutto <geoffroy.maurutto@gmail.com>
2024-06-19 13:41:51 +0200

    updt lesson

--- view/forum/listCategories.php
+++ view/forum/listCategories.php
@@ -9,3 +9,4 @@ foreach($categories as $category ){ ?>
 <p><a href="index.php?ctrl=forum&action=listTopicsByCategory&id=<?= $category->getId() ?>"><?= $category->getCategoryName() ?></a></p>
 <?php 
 }
+
