9d1532a6de6490b0ca63c00d4a341f7608fa4882
Geoffroy Maurutto <165882140+Jojoaventurier@users.noreply.github.com>
2024-06-20 20:59:40 +0200

    updated css

--- view/home.php
+++ view/home.php
@@ -1,11 +1,11 @@
 <h1>BIENVENUE SUR LE FORUM</h1>
 
-<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit ut nemo quia voluptas numquam, itaque ipsa soluta ratione eum temporibus aliquid, facere rerum in laborum debitis labore aliquam ullam cumque.</p>
+<p class="justified">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit ut nemo quia voluptas numquam, itaque ipsa soluta ratione eum temporibus aliquid, facere rerum in laborum debitis labore aliquam ullam cumque.</p>
 
 <p>
     <!--<a href="index.php?ctrl=security&action=login">Se connecter</a>-->
     <!-- <a href="index.php?ctrl=security&action=register">S'inscrire</a> -->
-    <a href="index.php?ctrl=forum&action=listTopics">Liste des topics</a>
     <a href="index.php?ctrl=forum&action=listTopicsByCategories">Liste des topics par catégorie</a>
+    <a href="index.php?ctrl=forum&action=listTopics">Liste des topics</a>
     <a href="index.php?ctrl=forum&action=listUsers">Liste des utilisateurs</a>
 </p>
