0077411f0341125225bbe6134cdc5f873ce73f80
Geoffroy Maurutto <165882140+Jojoaventurier@users.noreply.github.com>
2024-06-21 13:58:12 +0200

    updt

--- view/forum/listTopics.php
+++ view/forum/listTopics.php
@@ -17,7 +17,7 @@
                     <div class="topicCard">
                         <div class="topicTitle">
                             <h4><?=$topic->getTitle()?></h4>
-                            <p>créé par <?= $topic->getUser() ?>, le <?= $topic->getCreationDate(); // <?= $topic->getCategory()?></p>
+                            <p>créé par <span class='bold'><?= $topic->getUser() ?></span>, le <?= $topic->getCreationDate(); // <?= $topic->getCategory()?></p>
                             <p> Dernier message le : _______ par ________</p>
                             <p>APERCU DU DERNIER MESSAGE</p>
                         </div>  
